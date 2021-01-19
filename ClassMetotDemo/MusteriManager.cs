23ca6bd8297cc700ec4ca5d7faa4ef60eb55fd6f
mtgoktay <mtgoktay@gmail.com>
2021-01-19 19:22:29 +0300

    18 Ocak Güncelleme

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -11,34 +11,48 @@ namespace ClassMetotDemo
             Console.WriteLine(" Kaç kullanıcı eklemek istiyorsunuz");
             string kullaniciSayisi = Console.ReadLine();
             string[] musteriler = new string[100];
-              
-            for (int i = 0; i < int.Parse(kullaniciSayisi); i=i+2)
+            int k = 0;
+            
+            for (int i = 0; i < int.Parse(kullaniciSayisi); i++)
             {
-                int j;
-                j = i;
-                //j = j+2;
-                Musteri musteri1 = new Musteri();
-                Console.WriteLine(" Adını giriniz");
-                musteri1.Ad = Console.ReadLine();
-                Console.WriteLine("Soyadınızı giriniz");
-                musteri1.Soyad = Console.ReadLine();
-                musteri1.Id = j;
-                Console.WriteLine("Tebrikler" + " " + musteri1.Ad  + " " + musteri1.Soyad + " " + musteri1.Id + " " + "numaralı yeni kullanıcı eklendi");
-                //string[] musteriler = new string[100]; 
-                musteriler[i] = musteri1.Ad;
-                musteriler[j+1] = musteri1.Soyad;
-                Console.WriteLine("--------------------------------------------");
+                for (int m = i; m <= i; m = m+2)
+                {
+                    int j;
+                    j = m;
+                    k = k + 1;
+                    Musteri musteri1 = new Musteri();
+                    Console.WriteLine(" Adını giriniz");
+                    musteri1.Ad = Console.ReadLine();
+                    Console.WriteLine("Soyadınızı giriniz");
+                    musteri1.Soyad = Console.ReadLine();
+                    musteri1.Id = k;
+                    Console.WriteLine("Tebrikler" + " " + musteri1.Ad + " " + musteri1.Soyad + " " + musteri1.Id + " " + "numaralı yeni kullanıcı eklendi");
+                    //string[] musteriler = new string[100]; 
+                    musteriler[m] = musteri1.Ad;
+                    musteriler[j + 1] = musteri1.Soyad;
+                    Console.WriteLine("--------------------------------------------");
+                }
+                
                     
             }
-            int y = 0;
-            int z = 0;
-            for (int x = 0; x < int.Parse(kullaniciSayisi); x++)
+            Console.WriteLine("Kullanıcıları Listelemek İstermisiniz, Evet - Hayır");
+            string yanit = Console.ReadLine();
+            if (yanit == "Evet" || yanit == "evet")
             {
-                y = y + 1;
-                Console.WriteLine(y + " .ci müşteri" + " " +musteriler[z]);
-                z = z + 2;
+                int y = 0;
+                int z = 0;
+                for (int x = 0; x < (int.Parse(kullaniciSayisi)); x++)
+                {
+                    y = x;
+                    z = z + 1;
+                    Console.WriteLine(z + " " + " .ci müşteri" + " " + musteriler[y]);
+                }
             }
-
+            else
+            {
+                Console.WriteLine("Yanıt vermediniz");
+            }
+                                 
         }
 
 
