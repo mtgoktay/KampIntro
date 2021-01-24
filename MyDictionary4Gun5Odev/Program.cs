605688a1f2d99e0f4c24fcc61d7261955721805b
mtgoktay <mtgoktay@gmail.com>
2021-01-24 20:24:59 +0300

    24 ocak

---
+++ MyDictionary4Gun5Odev/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary4Gun5Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Mehmet GÖKTAY");
+            myDictionary.Add(2, "Engin Demiroğ");
+            myDictionary.Add(3, "Ali Sunal");
+
+            myDictionary.GetAll();
+        }
+    }
+}
---
+++ MyDictionary4Gun5Odev/StudentInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary4Gun5Odev
+{
+    class StudentInfo
+    {
+        public int Number { get; set; }
+        public string OgrenciAd { get; set; }
+        public string OgrenciSoyad { get; set; }
+        public string Class { get; set; }
+    }
+}
